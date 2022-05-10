43a8e6fb9d54d4dcdfcf7b7652e4a9c9cd42cdfc
xiaojuntao <find_onepiece@163.com>
2022-05-10 22:40:33 +0800

    feat: 模板语法

--- controllers/chapter03/temp01.go
+++ controllers/chapter03/temp01.go
@@ -9,5 +9,7 @@ type Temp01Controller struct {
 func (t *Temp01Controller) Get() {
 	t.Data["name"] = "xiao"
 	t.Data["arr"] = [3]int{1, 2, 3}
+	t.Data["html"] = `<a href="http://www.baidu.com">百度</a>`
+	t.Data["username"] = "xiaoxiao"
 	t.TplName = "chapter03/index.html"
 }
