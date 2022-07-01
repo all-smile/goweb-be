540c70df0afe6c2bae5cc1c2d996cb72b16d531f
xiaojuntao <find_onepiece@163.com>
2022-07-01 11:37:07 +0800

    feat: 首页增加接口路由展示

--- controllers/chapter03/temp01.go
+++ controllers/chapter03/temp01.go
@@ -11,5 +11,12 @@ func (t *Temp01Controller) Get() {
 	t.Data["arr"] = [3]int{1, 2, 3}
 	t.Data["html"] = `<a href="http://www.baidu.com">百度</a>`
 	t.Data["username"] = "xiaoxiao"
+	t.Data["X"] = 1
+	t.Data["Y"] = 2
+	t.Data["Z"] = 3
+	t.Data["arr"] = [3]int{1, 2, 3}
+	t.Data["mapdata"] = map[string]string{
+		"name": "xiao",
+	}
 	t.TplName = "chapter03/index.html"
 }
